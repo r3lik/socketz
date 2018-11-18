d92f54e00df5ff5df46e66ca6944eee3ee1aca30
Mike Czarny <mike@swatt.exchange>
2018-11-18 13:09:23 -0800

    init repo

---
+++ server.py
@@ -0,0 +1,79 @@
+import argparse
+import socket
+import threading
+import uuid
+
+def parse_args():
+    """ CLI  """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-H','--host', type=str, default='127.0.0.1')
+    parser.add_argument('-p','--port', type=str, default=5151)
+    result = parser.parse_args()
+    return result
+
+def threaded_client(client_list, conn, addr):
+    """ Threads single client connections"""
+    client_id = "{0}:{1}".format(addr[0],addr[1])
+    UUID=str(uuid.uuid4())
+    try:
+        client_list.add(client_id)
+
+        while True:
+
+            data = conn.recv(2048)
+
+            if not data:
+                break
+
+            elif data.decode('ASCII').strip() == "WHY":
+                reply = '42\n'
+                conn.sendall(reply.encode('ASCII'))
+                print("sent reply to {0}".format(client_id))
+
+            elif data.decode('ASCII').strip() == "WHO":
+                reply = "{0}\n".format(client_list)
+                conn.sendall(reply.encode('ASCII'))
+                print("sent reply to {0}".format(client_id))
+
+            elif data.decode('ASCII').strip() == "WHERE":
+                reply = "{0}\n".format(UUID)
+                conn.sendall(reply.encode('ASCII'))
+                print("sent reply to {0}".format(client_id))
+
+            elif data.decode('ASCII').strip() == "QUIT":
+                break
+
+            #print(data.encode('ASCII'))
+
+        conn.shutdown(socket.SHUT_RDWR)
+
+    except socket.error as err:
+        print("Socket disconnected...")
+
+    finally:
+        client_list.remove(client_id)
+        conn.close()
+
+def server(host, port, client_list):
+    print("Starting server...")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((host, port))
+    s.listen(5)
+    while True:
+        conn, addr = s.accept()
+        print('connected to: '+addr[0]+':'+str(addr[1]))
+        t = threading.Thread(target=threaded_client, args=(client_list, conn, addr))
+        t.daemon = True
+        t.start()
+
+def main():
+    client_list = set() # client must be unique
+    args = parse_args()
+    try:
+        server(args.host, args.port, client_list)
+    except KeyboardInterrupt:
+        print("Keyboard interrupt")
+
+if __name__ == '__main__':
+    main()
